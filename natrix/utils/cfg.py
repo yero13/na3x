c1f68cc8714645c254d92fa34d4b9963f6187263
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2018-03-18 22:40:42 +0200

    natrix -> na3x

--- natrix/utils/cfg.py
+++
@@ -1,7 +0,0 @@
-from string import Template
-
-
-class CfgUtils:
-    @staticmethod
-    def substitute_params(cfg, params):
-        return  Template(cfg).safe_substitute(params)
--- natrix/utils/object.py
+++
@@ -1,7 +0,0 @@
-def obj_for_name(obj_name):
-    parts = obj_name.split('.')
-    module = ".".join(parts[:-1])
-    m = __import__( module )
-    for comp in parts[1:]:
-        m = getattr(m, comp)
-    return m
